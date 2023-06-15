5132b33dd105858335791cd762530d250277ca68
Sander Faas <s.faas@gymnasiumbeekvliet.nl>
2023-06-15 21:18:05 +0200

    First try

---
+++ docs/source/conf.py
@@ -0,0 +1,30 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = 'Programmeren in Python'
+copyright = '2023, S. Faas'
+author = 'S. Faas'
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = ['hoverxref.extension']
+
+templates_path = ['_templates']
+exclude_patterns = ['.venv']
+
+language = 'nl'
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_theme = 'sphinx_rtd_theme'
+html_static_path = ['_static']
+
+# -- Options for hoverxref extension------------------------------------------
+hoverxref_roles = ['term']
